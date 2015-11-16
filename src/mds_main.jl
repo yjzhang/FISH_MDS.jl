38872c5bef3d99a524563f134082f8d9824a42fd
scriscio <steven.criscione@gmail.com>
2015-11-16 14:34:38 -0500

    Update mds_main.jl

--- src/mds_main.jl
+++ src/mds_main.jl
@@ -118,6 +118,7 @@ function parse_cl()
             default = ""
         "--radius", "-r"
             help = "Radius of chromosome (determined via imaging)"
+            arg_type = Number
             default = 1
         "--output", "-o"
             help = "Output file name"
